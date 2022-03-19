9c628a42251b392c5b8609a79311e8a184d5e838
nat corpuz <nate.corpuz1@gmail.com>
2022-03-19 21:01:46 +0800

    end modal

--- script.js
+++ script.js
@@ -1,72 +1,89 @@
-const getElements = (() => {
-    const cells = document.querySelectorAll('.cell');
-    return {cells};
-})();
-
-const createPlayer = (name) => {
-    const playerName = name;
-    return {playerName};
-}
-
 const Game = (() => {
-    const [...DOMboard] = getElements.cells;
+
+    let board = [,,,,,,,,,];
     const x = 'x';
     const o = 'o';
-    const board = [,,,,,,,,,];
-    let moveSwitcher = 0;
-    let winner;
-    const start = () => {
-        const Player = (() => {
-            const checkWinner = () => {
-                //x win conditions
-                if (board[0] == x && board[1] == x && board[2] == x) winner = x
-                else if (board[0] == x && board[3] == x && board[6] == x) winner = x
-                else if (board[0] == x && board[4] == x && board[8] == x) winner = x
-                else if (board[6] == x && board[7] == x && board[8] == x) winner = x
-                else if (board[2] == x && board[5] == x && board[8] == x) winner = x
-                else if (board[3] == x && board[4] == x && board[5] == x) winner = x
-                else if (board[1] == x && board[4] == x && board[7] == x) winner = x
-                else if (board[2] == x && board[4] == x && board[6] == x) winner = x
-                //o win conditions
-                else if (board[0] == o && board[1] == o && board[2] == o) winner = o
-                else if (board[0] == o && board[3] == o && board[6] == o) winner = o
-                else if (board[0] == o && board[4] == o && board[8] == o) winner = o
-                else if (board[6] == o && board[7] == o && board[8] == o) winner = o
-                else if (board[2] == o && board[5] == o && board[8] == o) winner = o
-                else if (board[3] == o && board[4] == o && board[5] == o) winner = o
-                else if (board[1] == o && board[4] == o && board[7] == o) winner = o
-                else if (board[2] == o && board[4] == o && board[6] == o) winner = o;
-                return winner;
-            }
-            const click = cell => {
-                cell.addEventListener('click', e => {
-                    const cellPos = e.target.dataset.pos;
-                    if (moveSwitcher) {
-                        e.target.classList.toggle(o);
-                        board.splice(cellPos,1,o)
-                        moveSwitcher = 0;
-                    }
-                    else {
-                        e.target.classList.toggle(x);
-                        board.splice(cellPos,1,x)
-                        moveSwitcher = 1;
-                    }
-                    winner = checkWinner();
-                });
-            }
-            return {click};
-        })();
-        DOMboard.forEach(Player.click);
+    let p1Score = 0;
+    let p2Score = 0;
+    let p1 = ''
+    let p2 = '';
+
+    const getElements = (() => {
+        const cells = document.querySelectorAll('.cell');
+        return {cells};
+    })();
+
+    const gameOver = () => {
+        const body = document.querySelector('body');
+        const modal = document.createElement('div');
+        const announce = document.createElement('h1');
+        const resetBtn = document.createElement('button');
+        resetBtn.innerText = 'Play Again';
+        resetBtn.classList.add('reset-btn');
+        modal.classList.add('modal-end');
+        announce.innerText = 'Game Over!';
+        body.appendChild(modal);
+        modal.appendChild(announce);
+        modal.appendChild(resetBtn);
+        resetBtn.addEventListener('click', () => {
+            modal.remove();
+            board = [,,,,,,,,,];
+            winner = '';
+            getElements.cells.forEach(cell => {
+                if(cell.classList.contains(x) || cell.classList.contains(o)) {
+                    cell.classList.remove(x);
+                    cell.classList.remove(o);
+                }
+            })
+        })
     }
 
-    const end = () => {
+    let winner = '';
 
+    const checkWin = () => {
+        if (board[0] == x && board[1] == x && board[2] == x) winner = x
+        else if (board[0] == x && board[3] == x && board[6] == x) winner = x
+        else if (board[0] == x && board[4] == x && board[8] == x) winner = x
+        else if (board[6] == x && board[7] == x && board[8] == x) winner = x
+        else if (board[2] == x && board[5] == x && board[8] == x) winner = x
+        else if (board[3] == x && board[4] == x && board[5] == x) winner = x
+        else if (board[1] == x && board[4] == x && board[7] == x) winner = x
+        else if (board[2] == x && board[4] == x && board[6] == x) winner = x
+        //o win conditions
+        else if (board[0] == o && board[1] == o && board[2] == o) winner = o
+        else if (board[0] == o && board[3] == o && board[6] == o) winner = o
+        else if (board[0] == o && board[4] == o && board[8] == o) winner = o
+        else if (board[6] == o && board[7] == o && board[8] == o) winner = o
+        else if (board[2] == o && board[5] == o && board[8] == o) winner = o
+        else if (board[3] == o && board[4] == o && board[5] == o) winner = o
+        else if (board[1] == o && board[4] == o && board[7] == o) winner = o
+        else if (board[2] == o && board[4] == o && board[6] == o) winner = o;
+        return winner;
     }
-    return {start, end};
+    let currentPlayer = 1;
+
+    const clickAction = cell => {
+        const cellPos = cell.target.dataset.pos;
+        if (cell.target.classList.contains('o') || cell.target.classList.contains('x')) {
+            return;
+        }
+        else if (currentPlayer) {
+            cell.target.classList.toggle('x');
+            board.splice(cellPos,1,'x');
+            currentPlayer = 0;
+        }
+        else {
+            cell.target.classList.toggle('o');
+            board.splice(cellPos,1,'o');
+            currentPlayer = 1;
+        }
+        winner = checkWin();
+        if (winner === x || winner === o) {
+            gameOver();
+        }
+    }
+
+    getElements.cells.forEach(cell => {
+        cell.addEventListener('click', clickAction)
+    })
 })();
-
-//if player presses start game
-Game.start();
-
-//if winner is not undefined
-Game.end();
